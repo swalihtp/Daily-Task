f8974bf5a33834422c97a4155868f1ac39260128
Swalih tp <swaliiihtp@gmail.com>
2025-08-11 12:19:51 +0530

    Destructuring

---
+++ task-5.js
@@ -0,0 +1,17 @@
+
+let marks=[{"a":1},{"b":2},{"c":3},{"d":4},{"e":5},{"f":6}];
+let [first,...rest]=marks;
+let [,,c]=marks;
+let [,,,d]=marks;
+console.log(first);
+console.log(rest);
+console.log(c);
+console.log(d);
+
+
+
+
+
+
+
+
