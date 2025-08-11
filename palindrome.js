65e797efeb4b61ec3a5df59824f87b1825c14d28
Swalih tp <swaliiihtp@gmail.com>
2025-08-11 17:12:28 +0530

    Palindrome Number

---
+++ palindrome.js
@@ -0,0 +1,17 @@
+let isPalindrome = function(x) {
+    let xcon=String(x)
+    let xstr="";
+    for(i=xcon.length-1;i>=0;i--){
+        xstr+=xcon[i]
+    }
+    
+    if(Number(xstr)===x){
+        return true
+    }else{
+        return false
+    }
+    
+};
+
+console.log(isPalindrome(121));
+
