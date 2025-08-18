86963902d3746f2b0a64df967f2570153b715281
Swalih tp <swaliiihtp@gmail.com>
2025-08-18 22:39:29 +0530

    Find the Index of the First Occurrence in a String

---
+++ task-6.js
@@ -0,0 +1,7 @@
+
+var strStr = function(haystack, needle) {
+  return haystack.indexOf(needle)  
+};
+console.log(strStr("HelloWorld","Hello"));
+console.log(strStr("HelloWorld","World"));
+console.log(strStr("HelloWorld","suii"));
