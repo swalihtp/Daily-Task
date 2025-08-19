1845dcfe73439eff214c3f464e32e6e6381fabd6
Swalih tp <swaliiihtp@gmail.com>
2025-08-19 23:16:38 +0530

    simple calculater

---
+++ simpleCalculater.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    return "Error: Cannot divide by zero";
+  }
+  return a / b;
+}
+
+console.log("Add: ", add(10, 5));
+console.log("Subtract: ", subtract(10, 5));
+console.log("Multiply: ", multiply(10, 5));
+console.log("Divide: ", divide(10, 5));
