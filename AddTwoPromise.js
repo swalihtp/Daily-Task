ccbabd1d6f946b37c4e1af61e5ca9a2e76e28e6d
Swalih tp <swaliiihtp@gmail.com>
2025-08-24 09:11:47 +0530

    Add Two Promises

---
+++ AddTwoPromise.js
@@ -0,0 +1,6 @@
+var addTwoPromises = async function(promise1, promise2) {
+    const value1=await promise1
+    const value2=await promise2
+    return value1+value2
+};
+console.log(addTwoPromises(29,23));
