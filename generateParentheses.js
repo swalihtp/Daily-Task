dec40bde281d4876821dab64e0d60032e2a2d79a
Swalih tp <swaliiihtp@gmail.com>
2025-08-30 18:56:43 +0530

    Generate Parantheses

---
+++ generateParentheses.js
@@ -0,0 +1,18 @@
+function Sample(n){
+    let result=[];
+
+    function mini(current,open,close){
+        if(current.length==n*2){
+            result.push(current) 
+            return;
+        }if(open<n){
+            mini(current+"(",open+1,close)
+        }if(close<open){
+            mini(current+")",open,close+1)
+        }
+        
+    }
+    mini("",0,0)
+    return result
+}
+console.log(Sample(3));
