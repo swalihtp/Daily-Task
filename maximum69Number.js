b496d5b29ee050f56c1359b4104eb534da1ef80b
Swalih tp <swaliiihtp@gmail.com>
2025-09-25 22:23:40 +0530

    Maximum 69 Number

---
+++ maximum69Number.js
@@ -0,0 +1,13 @@
+var maximum69Number  = function(num) {
+    nums=num.toString()
+    let ans=[]
+    for(let i=0;i<nums.length;i++){
+        num=nums.split("")
+        num[i]="9"
+        num=num.join("")
+        ans.push(Number(num))
+
+    }
+    return Math.max(...ans)
+};
+console.log(maximum69Number(9669))
