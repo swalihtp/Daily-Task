5354a89cfc9c9bc631fc32b2aa019f836ddd7a5f
Swalih tp <swaliiihtp@gmail.com>
2025-10-03 14:12:18 +0530

    number Of Employees Who Met Target

---
+++ numberOfEmployeesWhoMetTarget.js
@@ -0,0 +1,10 @@
+var numberOfEmployeesWhoMetTarget = function(hours, target) {
+    let count=0;
+    for(let i=0;i<hours.length;i++){
+        if(hours[i]>=target){
+            count++
+        }
+    }return count
+};
+console.log(numberOfEmployeesWhoMetTarget([0,1,2,3,4],2))
+console.log(numberOfEmployeesWhoMetTarget([5,1,4,2,2],6))
