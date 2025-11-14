380f9904e87b409bd6b3f126790fbc910f6d6290
Swalih tp <swaliiihtp@gmail.com>
2025-11-14 16:53:56 +0530

    Time: 2 ms (61.62%), Space: 57 MB (41.95%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+    const ans=[]
+    for(let i=0;i<nums1.length;i++){
+        if(nums2.includes(nums1[i])){
+            ans.push(nums1[i])
+            nums2.splice(nums2.indexOf(nums1[i]),1)
+        }
+    }
+    // let unq=new Set(ans)
+    // unq=Array.from(unq)
+    // return unq
+    return ans
+};
