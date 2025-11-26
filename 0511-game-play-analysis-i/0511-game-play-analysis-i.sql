bba462e53db02d95082fa3f6f1091d417e69ade2
Swalih tp <swaliiihtp@gmail.com>
2025-11-26 18:07:01 +0530

    Time: 456 ms (72.26%), Space: 0B (100%) - LeetHub

--- 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -1,3 +1,4 @@
-# Write your MySQL query statement below
-select distinct player_id,(select event_date from Activity where a.player_id=player_id order by event_date limit 1) as first_login from Activity a
-;
+select player_id,(
+    select min(event_date)
+) as first_login from Activity
+group by player_id
