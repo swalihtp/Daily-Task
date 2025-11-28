028deb15fde48b9018d578570f31c6f87c7ca357
Swalih tp <swaliiihtp@gmail.com>
2025-11-28 19:23:19 +0530

    Time: 0 ms (100%), Space: 56.1 MB (6.67%) - LeetHub

--- 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.js
+++ 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.js
@@ -3,18 +3,16 @@
  * @return {string}
  */
 var generateTheString = function(n) {
-    const a='a'
-    const b='b'
     let ans=""
     if(n%2==0){
         for(let i=0;i<n-1;i++){
-            ans+=a
+            ans+='a'
         }
-        ans+=b
+        ans+='b'
     }
     else{
         for(let i=n;i>0;i--){
-            ans+=a
+            ans+='a'
         }
         
     }
