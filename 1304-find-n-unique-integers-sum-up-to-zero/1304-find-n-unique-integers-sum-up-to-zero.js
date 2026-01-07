51a57a98c1d2f046f8a9a3427779e206ff7ac46e
Swalih tp <swaliiihtp@gmail.com>
2026-01-07 14:33:58 +0530

    Time: 0 ms (100%), Space: 56.2 MB (46.81%) - LeetHub

---
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var sumZero = function(n) {
+    const ans=[]
+    for(let i=1;i<=n;i++){
+        if(i===n){
+            const value=ans.reduce((total,value)=>{return total+value},0)
+            ans.push(-value)
+        }else{
+            ans.push(i)
+        }
+        
+    }
+    return ans
+};
