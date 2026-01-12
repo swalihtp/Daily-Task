4e00fbf73f68d86459482ba6360d80a7ce342f75
Swalih tp <swaliiihtp@gmail.com>
2026-01-12 22:42:07 +0530

    Time: 0 ms (100%), Space: 54.4 MB (63.33%) - LeetHub

---
+++ 2651-calculate-delayed-arrival-time/2651-calculate-delayed-arrival-time.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} arrivalTime
+ * @param {number} delayedTime
+ * @return {number}
+ */
+var findDelayedArrivalTime = function(arrivalTime, delayedTime) {
+    const t=arrivalTime+delayedTime
+    return t<24?t:t-24
+
+};
