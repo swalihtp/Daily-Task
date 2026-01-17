f9b1fddfaa4dccf010fe46fe2498be4aa3cab521
Swalih tp <swaliiihtp@gmail.com>
2026-01-17 20:29:45 +0530

    Time: 0 ms (100%), Space: 65.2 MB (24.39%) - LeetHub

---
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[][]} score
+ * @param {number} k
+ * @return {number[][]}
+ */
+var sortTheStudents = function(score, k) {
+    return score.sort((a,b)=>(b[k]-a[k]))
+    
+};
