ba59c06093eb9aa75c092cd29cdcd1229e94fdee
Swalih tp <swaliiihtp@gmail.com>
2026-01-25 12:21:56 +0530

    Time: 1 ms (41.75%), Space: 53.7 MB (77.67%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.js
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.js
@@ -3,12 +3,12 @@
  * @return {number}
  */
 var totalMoney = function(n) {
-    let mond = 1;   // Monday deposit
+    let mond = 1;  
     let money = 0;
-    let temp = 1;   // Current day's deposit
+    let temp = 1;  
     
     for (let i = 0; i < n; i++) {
-        if (i % 7 === 0) {   // New week starts
+        if (i % 7 === 0) {   
             money += mond;
             temp = mond;
             mond++;
