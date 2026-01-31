9105a91a75c5bb644f19829d59082354db3349cb
Swalih tp <swaliiihtp@gmail.com>
2026-01-31 16:29:24 +0530

    Time: 0 ms (100%), Space: 55.2 MB (8.06%) - LeetHub

--- 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.js
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.js
@@ -5,11 +5,8 @@
  */
 var createTargetArray = function(nums, index) {
     let ans = [];
-    for (let i = 0; i < nums.length; i++) {
-        const n1 = ans.slice(0, index[i]);
-        n1.push(nums[i]);
-        const n2 = ans.slice(index[i]);
-        ans = n1.concat(n2);
+    for(let i=0;i<nums.length;i++){
+        ans.splice(index[i],0,nums[i])
     }
-    return ans;
+    return ans
 };
