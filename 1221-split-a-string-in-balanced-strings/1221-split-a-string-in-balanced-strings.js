c89bedeabfbd9cb3b386b8e431010cef2de0faa1
Swalih tp <swaliiihtp@gmail.com>
2026-02-18 15:08:27 +0530

    Time: 2 ms (11.27%), Space: 54 MB (62.25%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.js
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.js
@@ -4,12 +4,15 @@
  */
 var balancedStringSplit = function(s) {
     let count=0
-    s=s.split("")
-    for(let i=0;i<s.length;i++){
-        let temp=s.slice(0,i+1)
-        const l=temp.filter((letter)=>letter==='L')
-        const r=temp.filter((letter)=>letter==='R')
-        if (l.length === r.length)count++
+    let balance=0
+    
+    for (let c of s){
+        if (c==='R'){
+            balance++
+        }else{
+            balance--
+        }
+        if(balance===0) count++
     }
     return count
 };
