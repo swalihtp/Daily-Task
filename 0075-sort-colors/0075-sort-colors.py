8f4a8c9f3b8a35f85927c1aecf167c0da23d8fea
Swalih tp <swaliiihtp@gmail.com>
2026-04-14 09:47:19 +0530

    Time: 0 ms (100%), Space: 19.3 MB (69.73%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,5 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
+        red=nums.count(0)
+        white = nums.count(1)
+        blue = nums.count(2)
+
+        for i in range(0,red):
+            nums[i]=0
+
+        for i in range(red,white+red):
+            nums[i]=1
+
+        for i in range(red+white,red+white+blue):
+            nums[i]=2
         
