34defb98cf4ec51781fb3f763871280dbd405aa2
Swalih tp <swaliiihtp@gmail.com>
2026-04-16 17:10:03 +0530

    Time: 0 ms (100%), Space: 19.2 MB (72.57%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.py
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.py
@@ -1,11 +1,9 @@
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
-        temp=[ch for ch in word if ch.islower()]
+        temp=set([ch for ch in word if ch.islower()])
         ans=0
-        checked=[]
         for i in temp:
-            if i.upper() in word and i not in checked:
-                checked.append(i)
+            if i.upper() in word:
                 ans+=1
         return ans
         
