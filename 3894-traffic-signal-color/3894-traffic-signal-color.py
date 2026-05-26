291aff29ef258b3c78fa4dad33620452370c0d7e
Swalih tp <swaliiihtp@gmail.com>
2026-05-26 15:58:17 +0530

    Time: 0 ms (100%), Space: 19.2 MB (88.25%) - LeetHub

--- 3894-traffic-signal-color/3894-traffic-signal-color.py
+++ 3894-traffic-signal-color/3894-traffic-signal-color.py
@@ -1,11 +1,10 @@
 class Solution:
     def trafficSignal(self, timer: int) -> str:
-        match timer:
-            case 0:
-                return 'Green'
-            case 30:
-                return "Orange"
-            case _ if timer > 30 and timer <= 90:
-                return "Red"
-            case _ :
-                return "Invalid"
+        if timer == 0:
+            return "Green"
+        elif timer == 30:
+            return "Orange"
+        elif timer > 30 and timer <= 90:
+            return "Red"
+        else:
+            return "Invalid"
