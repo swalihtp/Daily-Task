79dec48e8557ffbe6ed5fd931b5a2dc5aca26875
Swalih tp <swaliiihtp@gmail.com>
2026-05-30 11:59:23 +0530

    Time: 102 ms (5.91%), Space: 27.4 MB (42.19%) - LeetHub

---
+++ 2512-reward-top-k-students/2512-reward-top-k-students.py
@@ -0,0 +1,34 @@
+class Solution:
+    def topStudents(self, positive_feedback: List[str], negative_feedback: List[str], report: List[str], student_id: List[int], k: int) -> List[int]:
+        positive_feedback = set(positive_feedback)
+        negative_feedback = set(negative_feedback)
+        students = {}
+        for i in range(len(student_id)):
+            st_id =  student_id[i]
+            rpt = report[i].split()
+
+            for word in rpt:
+                if word in positive_feedback:
+                    if student_id[i] in students:
+                        students[student_id[i]] =  students[student_id[i]]+3
+
+                    else:
+                        students[student_id[i]] = 3
+                elif word in negative_feedback:
+                    if student_id[i] in students:
+                        students[student_id[i]] =  students[student_id[i]]-1
+
+                    else:
+                        students[student_id[i]] = -1
+                else:
+                    if student_id[i] in students:
+                        students[student_id[i]] =  students[student_id[i]]
+                    else:
+                        students[student_id[i]] = 0
+
+        s = list(sorted(students.items(), key=lambda item:(-item[1],item[0])))[:k]
+        ans=[]
+
+        for i in s:
+            ans.append(i[0])
+        return ans
