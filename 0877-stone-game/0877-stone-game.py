f87617efdba74602414a2535f044598270710ea4
Swalih tp <swaliiihtp@gmail.com>
2026-08-02 21:45:18 +0530

    Time: 0 ms (100%), Space: 19.1 MB (99.37%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -1,12 +1,3 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        return True
-
-
-
-
-                
-            
-            
-
-        
+        return len(piles)%2 == 0
